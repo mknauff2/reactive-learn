3b9a623d376adf9cc2ea985211c09aaa0a265170
Mike Knauff <mknauff2@flash.net>
2017-10-01 08:48:03 -0500

    Initial commit of source

---
+++ reactive-learn/src/main/java/com/knauff/mike/reactive/ReactiveLearnApplication.java
@@ -0,0 +1,12 @@
+package com.knauff.mike.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveLearnApplication.class, args);
+	}
+}
---
+++ reactive-learn/src/test/java/com/knauff/mike/reactive/ReactiveLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knauff.mike.reactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
